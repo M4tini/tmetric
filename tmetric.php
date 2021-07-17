6e879f2a791d20220caec4e0790cc1cf0b5f419b
Martin Boer <martin@myparcel.com>
2021-07-17 02:28:21 +0200

    :bug: dynamic user when deleting time entries

--- tmetric.php
+++ tmetric.php
@@ -1,20 +1,12 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/config.php';
 
-Dotenv\Dotenv::createImmutable(__DIR__)->load();
+$config = new Config();
 
-$httpClient = new GuzzleHttp\Client([
-    'base_uri' => 'https://app.tmetric.com/api/v3/accounts/' . $_ENV['TMETRIC_WORKSPACE_ID'] . '/',
-]);
-$headers = [
-    'Authorization' => 'Bearer ' . $_ENV['TMETRIC_TOKEN'],
-];
-
-$response = match ($_POST['action']) {
-    'post' => $httpClient->post('timeentries?' . http_build_query(['userId' => $_ENV['TMETRIC_USER_ID']]), [
-        GuzzleHttp\RequestOptions::HEADERS => $headers,
-        GuzzleHttp\RequestOptions::JSON    => [
+$response = match ($config->action) {
+    'post' => $config->getTMetricClient()->post('v3/accounts/' . $config->tmetric_workspace_id . '/timeentries?' . http_build_query(['userId' => $config->tmetric_user_id]), [
+        GuzzleHttp\RequestOptions::JSON => [
             'project'   => [
                 'id' => $_POST['project'],
             ],
@@ -23,9 +15,8 @@ $response = match ($_POST['action']) {
             'endTime'   => $_POST['date'] . 'T' . $_POST['end'],
         ],
     ]),
-    'put' => $httpClient->put('timeentries/' . $_POST['id'], [
-        GuzzleHttp\RequestOptions::HEADERS => $headers,
-        GuzzleHttp\RequestOptions::JSON    => [
+    'put' => $config->getTMetricClient()->put('v3/accounts/' . $config->tmetric_workspace_id . '/timeentries/' . $_POST['id'], [
+        GuzzleHttp\RequestOptions::JSON => [
             'project'   => [
                 'id' => $_POST['project'],
             ],
@@ -35,9 +26,8 @@ $response = match ($_POST['action']) {
         ],
     ]),
     // For some reason, the delete request documented in their swagger does not delete anything, so we simulate the app.
-    'delete' => $httpClient->post('https://app.tmetric.com/api/accounts/97840/timeentries/128921/bulk', [
-        GuzzleHttp\RequestOptions::HEADERS => $headers,
-        GuzzleHttp\RequestOptions::JSON    => [
+    'delete' => $config->getTMetricClient()->post('accounts/' . $config->tmetric_workspace_id . '/timeentries/' . $config->tmetric_user_id . '/bulk', [
+        GuzzleHttp\RequestOptions::JSON => [
             [
                 'startTime' => $_POST['date'] . 'T' . $_POST['start'],
                 'endTime'   => $_POST['date'] . 'T' . $_POST['end'],
@@ -49,6 +39,6 @@ $result = json_decode((string) $response->getBody(), true);
 
 var_dump($result);
 
-echo '<a href="https://app.tmetric.com/#/tracker/' . $_ENV['TMETRIC_WORKSPACE_ID'] . '/?day=' . str_replace('-', '', $_POST['date']) . '" target="_blank">check</a>';
+echo '<a href="https://app.tmetric.com/#/tracker/' . $config->tmetric_workspace_id . '/?day=' . str_replace('-', '', $_POST['date']) . '" target="_blank">check</a>';
 echo '<style>body { cursor: crosshair; height: 100%; margin: 0; }</style>';
 echo '<script>document.body.onclick = () => window.close()</script>';
